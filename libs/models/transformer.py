d42c38c9a05b9fb7a49201f0dc78db7a8a150ca8
Blaise Gauvin St-Denis <bstdenis@gmail.com>
2020-03-31 19:13:38 -0400

    Adapt evaluator for the subword tutorial tokenizer

--- libs/models/transformer.py
+++ libs/models/transformer.py
@@ -407,6 +407,35 @@ class Transformer(tf.keras.Model):
             print(
                 'Time taken for 1 epoch: {} secs\n'.format(time.time() - start))
 
+    def evaluate(self, encoder_input, transformer_output, end_token):
+        # This limit in i should be MAX_LENGTH, but no longer using it so...
+        for slen in range(100):
+            enc_padding_mask, combined_mask, dec_padding_mask = \
+                create_masks(encoder_input, transformer_output)
+
+            # predictions.shape == (batch_size, seq_len, vocab_size)
+            predictions, attention_weights = self(encoder_input,
+                                                  transformer_output,
+                                                  False,
+                                                  enc_padding_mask,
+                                                  combined_mask,
+                                                  dec_padding_mask)
+
+            # select the last word from the seq_len dimension
+            predictions = predictions[:, -1:, :]  # (batch_size, 1, vocab_size)
+
+            predicted_id = tf.cast(tf.argmax(predictions, axis=-1), tf.int32)
+
+            # return the result if the predicted_id is equal to the end token
+            if predicted_id == end_token:
+                return tf.squeeze(transformer_output, axis=0), attention_weights
+
+            # concatentate the predicted_id to the output which is given to the
+            # decoder as its input.
+            transformer_output = tf.concat([transformer_output, predicted_id], axis=-1)
+
+        return tf.squeeze(transformer_output, axis=0), attention_weights
+
 
 class CustomSchedule(tf.keras.optimizers.schedules.LearningRateSchedule):
     def __init__(self, d_model, warmup_steps=4000):
