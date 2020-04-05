Bug fix on MLM masking logic

libs/data_loaders/abstract_dataloader_huggingfaces.py
@@ -167,16 +167,31 @@ class AbstractHuggingFacesTokenizer(AbstractDataloader, ABC):
 
     @staticmethod
     def _apply_mask_for_mlm(ds: tf.data.Dataset,
-                            distrib_mask: tfp.distributions.Multinomial,
+                            distrib_mask_actions: tfp.distributions.Multinomial,
                             distrib_random: tfp.distributions.Uniform,
                             with_multi_inputs=True):
+        """
+            Apply mask for masked language model
+
+        Args:
+            ds: dataset
+            distrib_mask_actions: Multinomial distribution for 4 class
+                3 for mask actions: do nothing, random token replacement, mask replacement.
+                1 for doing nothing on inputs but mask outputs
+            distrib_random: Uniform distribution of vocab size (without special tokens
+            with_multi_inputs:
+
+        Returns:
+
+        """
 
         # Inspiration from https://www.tensorflow.org/guide/data#applying_arbitrary_python_logic
         def _apply_mask_eager(inputs, output):
             input_shape = tf.shape(inputs)  # Shape Seq Length
+            output_shape = tf.shape(output)  # Shape Seq Length
 
             # TODO set seed
-            masks = distrib_mask.sample(input_shape, seed=42)  # Shape Seq Length * Probability for each class (3)
+            masks = distrib_mask_actions.sample(input_shape, seed=42)  # Shape Seq Length * Probability for each class (4)
             masks = tf.cast(masks, dtype=tf.int32)
             random_tokens = distrib_random.sample(input_shape, seed=42)  # TODO set seed
             random_tokens = tf.cast(random_tokens, dtype=tf.int32)
@@ -188,7 +203,9 @@ class AbstractHuggingFacesTokenizer(AbstractDataloader, ABC):
             # Replace with random token
             inputs_masked = tf.where(tf.math.equal(masks[:, 1], 1), inputs_masked, random_tokens)
 
-            return inputs_masked, output
+            output_masked = tf.where(tf.math.equal(masks[:, 3], 1), output, tf.ones(output_shape, dtype=tf.int32))
+
+            return inputs_masked, output_masked
 
         def apply_mask(x, y):
             inputs, attention_masks, tokens_type_ids = x

libs/data_loaders/dataloader_bilingual_subword_level.py
@@ -138,13 +138,20 @@ class BilingualTranslationLMDataloaderSubword(AbstractBilingualDataloaderSubword
             yield ((inputs, attention_masks, tokens_type_ids), output)
 
     def _hook_dataset_post_precessing(self, ds: tf.data.Dataset):
+        # Do action only for 15% of tokens (and mask output for others)
+        prob_mask_idx = 0.15
         # 10% nothing to do, 10% random word, 80% mask
-        distrib_mask = tfp.distributions.Multinomial(total_count=1, probs=[0.1, 0.1, 0.8])
+        # prob_nothing, prob_random_replacement, prob_replace_by_mask \
+        prob_mask_actions = np.array([0.1, 0.1, 0.8])
+        prob_mask_actions = prob_mask_actions * prob_mask_idx
+        prob_mask_actions = np.append(prob_mask_actions, [1 - sum(prob_mask_actions)]).tolist()
+        distrib_mask = tfp.distributions.Multinomial(total_count=1,
+                                                     probs=prob_mask_actions)
 
         distrib_random = tfp.distributions.Uniform(low=len(self._special_tokens), high=self._vocab_size_source)
 
         return self._apply_mask_for_mlm(ds=ds,
-                                        distrib_mask=distrib_mask,
+                                        distrib_mask_actions=distrib_mask,
                                         distrib_random=distrib_random)
 
     def decode(self, tokens: List[int]):
@@ -205,13 +212,20 @@ class BilingualCustomPretrainingDataloaderSubword(AbstractBilingualDataloaderSub
     #         yield ((inputs, attention_masks, tokens_type_ids, tf.convert_to_tensor(isinstance())), output)
 
     def _hook_dataset_post_precessing(self, ds: tf.data.Dataset):
+        # Do action only for 15% of tokens (and mask output for others)
+        prob_mask_idx = 0.15
         # 10% nothing to do, 10% random word, 80% mask
-        distrib_mask = tfp.distributions.Multinomial(total_count=1, probs=[0.1, 0.1, 0.8])
+        # prob_nothing, prob_random_replacement, prob_replace_by_mask \
+        prob_mask_actions = np.array([0.1, 0.1, 0.8])
+        prob_mask_actions = prob_mask_actions * prob_mask_idx
+        prob_mask_actions = np.append(prob_mask_actions, [1 - sum(prob_mask_actions)]).tolist()
+        distrib_mask = tfp.distributions.Multinomial(total_count=1,
+                                                     probs=prob_mask_actions)
 
         distrib_random = tfp.distributions.Uniform(low=len(self._special_tokens), high=self._vocab_size_source)
 
         return self._apply_mask_for_mlm(ds=ds,
-                                        distrib_mask=distrib_mask,
+                                        distrib_mask_actions=distrib_mask,
                                         distrib_random=distrib_random)
 
     def decode(self, tokens: List[int]):