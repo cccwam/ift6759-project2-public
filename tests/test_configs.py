9c05c742fccc5c23ffdf7959e4b37007de797006
Francois Mercier <cccwam@gmail.com>
2020-04-08 19:43:23 -0400

    Fix one CircleCI test

--- tests/test_configs.py
+++ tests/test_configs.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 
 import jsonschema
 
@@ -7,7 +8,7 @@ from libs import helpers
 
 def validate_configs(configs_folder):
     schema = helpers.load_dict(os.path.join(configs_folder, 'schema.json'))
-    for file_name in os.listdir(configs_folder):
+    for file_name in Path(configs_folder).glob('**.json'):
         if file_name == 'schema.json':
             continue
         config_file_path = os.path.join(configs_folder, file_name)
