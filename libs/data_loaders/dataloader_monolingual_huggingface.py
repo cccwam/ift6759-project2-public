Bug fix on masked language model dataloader Bug fix on numerical stability for mlm_loss

libs/data_loaders/dataloader_monolingual_huggingface.py
@@ -2,7 +2,9 @@ from abc import ABC
 from functools import partial
 from typing import List
 
+import numpy as np
 import tensorflow as tf
+import tensorflow_probability as tfp
 from tokenizers import (Encoding)
 from tokenizers.implementations import BaseTokenizer
 
@@ -101,7 +103,7 @@ class MonolingualMaskedLanguageModelHF(AbstractMonolinguaHFlDataloaderSubword):
 
     def _hook_dataset_post_precessing(self, ds: tf.data.Dataset, batch_size: int):
         ds = ds.padded_batch(batch_size=batch_size,
-                             padded_shapes=[None])
+                             padded_shapes=([None], [None]))
         ds = ds.prefetch(tf.data.experimental.AUTOTUNE)
 
         def add_padding_mask(source):
@@ -112,6 +114,60 @@ class MonolingualMaskedLanguageModelHF(AbstractMonolinguaHFlDataloaderSubword):
 
         # For masked language model task, all datasets are masked
         return self._apply_mask_for_mlm(ds=ds, vocab_size=self._vocab_size)
+
+    def _apply_mask_for_mlm(self,
+                            ds: tf.data.Dataset,
+                            vocab_size: int):
+        """
+            Apply mask for masked language model
+
+        Args:
+            ds: dataset
+            vocab_size: vocab size
+
+        Returns:
+
+        """
+
+        # Do action only for 15% of tokens (and mask output for others)
+        prob_mask_idx = 0.15
+        # 10% nothing to do, 10% random word, 80% mask
+        # prob_nothing, prob_random_replacement, prob_replace_by_mask \
+        prob_mask_actions = np.array([0.1, 0.1, 0.8])
+        prob_mask_actions = prob_mask_actions * prob_mask_idx
+        prob_mask_actions = np.append(prob_mask_actions, [1 - sum(prob_mask_actions)]).tolist()
+
+        distrib_mask = tfp.distributions.Multinomial(total_count=1,
+                                                     probs=prob_mask_actions)
+
+        @tf.function
+        def apply_mask(x, output):
+            inputs, enc_padding_mask = x
+
+            input_shape = tf.shape(inputs)  # Batch size * Seq Length
+            output_shape = tf.shape(output)  # Batch size * Seq Length
+
+            masks = distrib_mask.sample(input_shape,
+                                        seed=self._seed)  # Batch size *Seq Length * Probability for each class (4)
+            masks = tf.cast(masks, dtype=tf.int32)
+
+            random_tokens = tf.random.uniform(input_shape, minval=len(self._special_tokens), maxval=vocab_size,
+                                              dtype=inputs.dtype, seed=self._seed, name=None)
+
+            # Replace with mask
+            # One is the mask token id for HuggingFace tokenizers
+            inputs_masked = tf.where(tf.math.equal(masks[:, :, 2], 1), inputs, tf.ones(input_shape, dtype=inputs.dtype))
+
+            # Replace with random token
+            inputs_masked = tf.where(tf.math.equal(masks[:, :, 1], 1), inputs_masked, random_tokens)
+
+            output_masked = tf.where(tf.math.equal(masks[:, :, 3], 1),
+                                     tf.zeros(output_shape, dtype=output.dtype),
+                                     output)
+
+            return (inputs_masked, enc_padding_mask), output_masked
+
+        return ds.map(map_func=apply_mask)
 
     @staticmethod
     def _my_generator_from_encodings(source_numericalized: List[Encoding]):

libs/losses.py
@@ -3,8 +3,10 @@ import tensorflow as tf
 logger = tf.get_logger()
 
 
+@tf.function
 def mlm_loss(y_true, y_pred):
     mask = tf.math.logical_not(tf.math.equal(y_true, 0))  # Batch size * Seq Length
+
     loss_ = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True, reduction='none')(y_true, y_pred)
 
     mask = tf.cast(mask, dtype=loss_.dtype)
@@ -13,7 +15,14 @@ def mlm_loss(y_true, y_pred):
     # Prevent averaging over non masked tokens
     numerator = tf.reduce_sum(loss_, axis=1)
     denominator = tf.reduce_sum(mask, axis=1)
-
+    # For numerical stability
+    denominator = tf.clip_by_value(denominator, clip_value_min=1.,
+                                   clip_value_max=tf.cast(tf.reduce_sum(tf.shape(y_true)), dtype=denominator.dtype))
     loss = numerator / denominator
+
+    # For Debug only
+    # if tf.math.is_nan(tf.reduce_mean(loss)):
+    #     logger.error("Nan value in loss")
+    #     tf.print(y_pred)
 
     return tf.reduce_mean(loss)  # Average over samples

libs/models/helpers.py
@@ -32,7 +32,7 @@ def load_pretrained_layers(config: dict, my_model: tf.keras.Model):
                         pretrained_encoder.enc_layers[i].layernorm2.get_weights())
                     decoder.dec_layers[i].layernorm3.set_weights(
                         pretrained_encoder.enc_layers[i].layernorm2.get_weights())
-
+            # TODO test with pretrained final layers
             else:
                 for l in pretrained_layer['layer_name'].split("/"):
                     pretrained_model = pretrained_model.get_layer(l)

libs/models/transformers_mt_encoder_decoder_v1.py
@@ -41,7 +41,7 @@ def builder(
         num_hidden_layers, hidden_size, num_attention_heads, intermediate_size, vocab_size_target,
         seq_length_target, dropout_rate=dropout_rate)
 
-    final_layer = tf.keras.layers.Dense(vocab_size_target)
+    final_layer = tf.keras.layers.Dense(vocab_size_target, name="final_layer")
 
     enc_inp = tf.keras.layers.Input(
         shape=(None,), dtype=tf.int32, name="enc_inp")